1448b2db0d5e3bc5d40fbb48091b5fc4b53230d7
David Branton <david.branton@candide.eu>
2020-04-24 21:49:36 +0100

    Updates

---
+++ precache-manifest.139aae38aee724fb8c92b1372e4fa806.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429b5b090091ddc3ee36031e3942d5dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d585ac7f782ce5e65815",
+    "url": "/static/js/2.50546014.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.50546014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72596f115e65b30e526b",
+    "url": "/static/js/main.a479fa2a.chunk.js"
+  },
+  {
+    "revision": "430711db2cc41196af7a",
+    "url": "/static/js/runtime-main.9798f641.js"
+  }
+]);
