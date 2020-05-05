bba984bd70ad377e8dc80fc17d800f1080a7cb89
BrantApps <oceanlife.development@gmail.com>
2020-05-05 14:26:03 +0000

    Deploying to gh-pages from  @ bfdb4acfec40c0467a330014de46f42bb1011bdd 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog/precache-manifest.a42a33ab55893ea6675355214de0154f.js"
+  "/blog/precache-manifest.95408f93350172f8347ba6153bc72211.js"
 );
 
 self.addEventListener('message', (event) => {
